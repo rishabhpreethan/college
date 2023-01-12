38b67d9e3fa6f294446815d7977b925af2254fa2
rishabhpreethan <rishabh.preethan@gmail.com>
2023-01-12 21:03:34 +0530

    committed

---
+++ sem_5/daa/1.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int partition(int a[], int low, int high){ 
+    int pivot=a[low], i=low, j=high+1;
+    int temp;    
+    while(i<j){
+        do{     
+            i++;
+        }while(pivot>=a[i] && i<high); 
+
+        do{     
+            j--; 
+        }while(pivot<a[j]); 
+        if(i<j){    
+            temp=a[i];    
+            a[i]=a[j];    
+            a[j]=temp; 
+        }    
+    }    
+    a[low]=a[j];    
+    a[j]=pivot;    
+    return j;
+}
+void quick_sort(int a[], int low, int high){    
+    int s;    
+    if(low<high){ 
+        s=partition(a,low,high); 
+        quick_sort(a,low,s-1); 
+        quick_sort(a,s+1,high);    
+    }
+}
+int main(){    
+    int a[10000],n,low,high,i;    
+    clock_t st, end;    
+    printf("Enter number of elements\n");    
+    scanf("%d",&n);    
+    printf("Random numbers generated are\n");    
+    for(i=0;i<n;i++){ 
+        a[i]=rand()%100;     
+        printf("%d\t",a[i]);    
+    }    
+    low=0;    
+    high=n-1;    
+    st=clock();    
+    quick_sort(a,low,high);    
+    end=clock();    
+    printf("\nSorted array\n");    
+    for(i=0;i<n;i++){ 
+        printf("%d\t",a[i]);    
+    }
+    printf("\nTime required to sort given elements is %f",(float)(end-st)/CLOCKS_PER_SEC);
+}
---
+++ sem_5/daa/2.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void simple_merge(int a[],int low, int mid, int high){    
+    int i=low, j=mid+1, k=low, c[10000];    
+    while(i<=mid && j<=high){ 
+        if(a[i]<a[j]) {     
+            c[k]=a[i];     
+            i++;     
+            k++; 
+        } 
+        else{     
+            c[k]=a[j];     
+            j++;     
+            k++; 
+        }    
+    }    
+    while(i<=mid) 
+        c[k++]=a[i++];    
+    while(j<=high) 
+        c[k++]=a[j++];
+    for(i=low;i<=high;i++) 
+        a[i]=c[i];
+}
+void merge_sort(int a[],int low,int high){    
+    int mid;    
+    if(low<high){ 
+        mid=(low+high)/2; 
+        merge_sort(a,low,mid); 
+        merge_sort(a,mid+1,high); 
+        simple_merge(a,low,mid,high);    
+    }
+}
+int main(){    
+    int a[10000],i=0,n;    
+    clock_t st,end;    
+    printf("Enter the value of n\n");    
+    scanf("%d",&n);    
+    printf("Random numbers generated are\n");    
+    for(i=0;i<n;i++){ 
+        a[i]=rand()%100; 
+        printf("%d\t",a[i]);    
+    }    
+    st=clock();    
+    merge_sort(a,0,n-1);
+    end=clock();    
+    printf("\nAfter Sorting\n");    
+    for(i=0;i<n;i++)    
+        printf("%d\t",a[i]);    
+    printf("\nTime required to sort given elements is %f",(float)(end-st)/CLOCKS_PER_SEC);
+}
