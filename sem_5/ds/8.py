597f58be836486f68c8f25167f18b76828744963
rishabhpreethan <rishabh.preethan@gmail.com>
2023-01-12 23:41:06 +0530

    committed

--- sem_5/ds/8.py
+++ sem_5/ds/8.py
@@ -4,10 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier 
 from sklearn import metrics
 import seaborn as sns
+
 pima=pd.read_csv("diabetes.csv")
 X = pima.iloc[:, [0,7]].values 
 y= pima.iloc[:, 8].values
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
 clf = DecisionTreeClassifier()
 y_pred = clf.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred)) 
