3b04a41bd911a54857662f59c458e783829b3318
rishabhpreethan <rishabh.preethan@gmail.com>
2023-01-21 22:54:20 +0530

    committed

--- sem_5/daa/7.c
+++ sem_5/daa/7.c
@@ -1,45 +1,58 @@
 // FLOYD WARSHALL'S ALGORITHM
 
-#include<stdio.h>
-
-int min(int a,int b){ 
-    if(a<b)   
-        return(a);
-    else   
-        return(b);
+#include <stdio.h>
+int min(int a, int b) {
+  if (a < b) return a;
+  else {
+    return b;
+  }
 }
 
-void floyds(int p[10][10],int n){    
-    int i,j,k; 
-    for(k=1;k<=n;k++)
-        for (i=1;i<=n;i++)     
-            for (j=1;j<=n;j++)
-                if(i==j)                   
-                    p[i][j]=0;            
-                else                
-                    p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
+void floyd(int arr[10][10], int n) {
+  int i,j,k;
+  for (k = 1; k <= n; k++) {
+    for (i = 1; i <= n; i++) {
+      for (j = 1; j <= n; j++) {
+        if (i == j) arr[i][j] = 0;
+        else {
+          arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+        }
+
+      }  
+    }
+  }
 }
 
-int main(){ 
-    int p[10][10],n,i,j; 
-    printf("Enter the number of vertices:\n"); 
-    scanf("%d",&n); printf("Enter the Matrix\n"); 
-    for(i=1;i<=n;i++){     
-        for(j=1;j<=n;j++){           
-            scanf("%d", &p[i][j]);     
-        } 
-    } 
-    floyds(p,n); 
-    printf("Output Matrix :\n"); 
-    for(i=1;i<=n;i++){  
-        for(j=1;j<=n;j++)     
-            printf("%d \t",p[i][j]);  
-            printf("\n"); 
-    } 
-    printf("\n The shortest paths are:\n"); 
-    for (i=1;i<=n;i++)     
-        for (j=1;j<=n;j++){      
-            if(i!=j)          
-                printf("\n <%d,%d>=%d",i,j,p[i][j]);     
-        } 
+int main() {
+  int arr[10][10], n, i, j;
+  printf("Enter number of vertices : \n");
+  scanf("%d", &n);
+  printf("\n Enter matrix : \n");
+
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      scanf("%d", &arr[i][j]);
+    }
+  }
+  floyd(arr, n);
+  printf("Output matrix : \n");
+
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      printf("%d \t", arr[i][j]);
+    }
+    printf("\n");
+  }
+
+  // shortest path (output matrix except the same i and j)
+  printf("Shortest paths are : \n");
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      if (i != j) { // print all except i = j (1,1 || 2,2 etc)
+        printf("\n <%d %d> =  %d", i, j, arr[i][j]);
+      }
+    }
+  }
+
+  return 0;
 }
