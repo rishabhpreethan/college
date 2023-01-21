bdca025aba3adc4977f4411b1251a8eef75f1fa6
rishabhpreethan <rishabh.preethan@gmail.com>
2023-01-21 17:54:44 +0530

    committed

--- sem_5/daa/3.c
+++ sem_5/daa/3.c
@@ -1,55 +1,44 @@
-// TOPOLOGICAL SORTING
+#include <stdio.h>
+int main() {
+  int count = 0;
+  int arr[10][10];
+  printf("\n Enter number of vertex : ");
+  int n;
+  scanf("%d", &n);
+  printf("\n Adjacency Matrix : ");
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      scanf("%d", &arr[i][j]);
+    }
+  }
+  int indegree[10], vis[10];
+  // set indegree and visited to 0
+  for (int i = 0; i < n; i++) {
+    indegree[i] = 0;
+    vis[i] = 0;
+  }
+  // calculate indegree
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      indegree[i] += arr[j][i];
+    }
+  }
 
-#include<stdio.h>
-
-void ts(int a[20][20],int n){    
-    int t[10],vis[10],stack[10],i,j,indeg[10],top=0,ele,k=1;    
-    for(i=1;i<=n;i++){ 
-        t[i]=0; vis[i]=0; 
-        indeg[i]=0;    
-    }    
-    for(i=1;i<=n;i++){ 
-        for(j=1;j<=n;j++){     
-            if(a[i][j]==1){  
-                indeg[j]=indeg[j]+1;     
-            } 
-        }    
-    }    
-    printf("Indegree Array:");    
-    for(i=1;i<=n;i++) 
-        printf("%d ",indeg[i]);    
-        for(i=1;i<=n;i++){ 
-            if(indeg[i]==0){      
-                stack[++top]=i;     
-                vis[i]=1;   
-            }    
-        }   
-        while(top>0){
-            ele=stack[top--]; 
-            t[k++]=ele; 
-            for(j=1;j<=n;j++){    
-                if(a[ele][j]==1 && vis[j]==0){  
-                    indeg[j]=indeg[j]-1;  
-                    if(indeg[j]==0){      
-                        stack[++top]=j;      
-                        vis[j]=1;  
-                    }     
-                }    
-            }    
-        }    
-        printf("\nTopological Ordering is:");    
-        for(i=1;i<=n;i++) printf("%d",t[i]);
-}
-
-int main(){    
-    int n,a[20][20],i,j;    
-    printf("Enter the number of nodes\n");    
-    scanf("%d",&n);    
-    printf("Enter Adjacency matric\n");    
-    for(i=1;i<=n;i++){ 
-        for(j=1;j<=n;j++){     
-            scanf("%d",&a[i][j]); 
-        }    
-    }    
-    ts(a,n);
+  // check
+  int k;
+  while (count < n) {
+    for (int k = 0; k < n; k++) {
+      if (indegree[k] == 0 && vis[k] == 0) {
+        printf("%d ", (k + 1));
+        vis[k] = 1;
+      }
+      // update indegree
+      for (int i = 0; i < n; i++) {
+        if (arr[i][k] == 1) { // if already visited that indegree
+          indegree[k]--;
+        }
+      }
+    }
+    count++;
+  }
 }
