70d078be62e64e989f55647a963e781e7d91524a
rishabhpreethan <rishabh.preethan@gmail.com>
2023-01-22 13:10:28 +0530

    committed

--- sem_5/daa/10.c
+++ sem_5/daa/10.c
@@ -1,53 +1,66 @@
 // N-QUEENS
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int board[20], count;
+char board[20][20];
+int n;
 
-void print(int n){    
-    int i, j;    
-    printf("\n\nSolution %d:\n\n",++count);    
-    for(i=1;i<=n;i++)        
-    printf("\t%d",i);    
-    for(i=1;i<=n;i++)    {        
-        printf("\n\n%d",i);        
-        for(j=1;j<=n;j++)         {            
-            if(board[i]==j)                
-                printf("\tQ");             
-            else                
-                printf("\t-");         
-        }    
+void display() {
+  printf("\n");
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%c \t", board[i][j]);
     }
+    printf("\n\n");
+  }
 }
 
-int place(int row,int column){    
-    int i;    
-    for(i=1;i<=row-1;i++)    {        
-        if(board[i]==column)            
-            return 0;        
-        else if(abs(board[i]-column)==abs(i-row))            
-            return 0;    
-    }    
-    return 1; 
-}
-
-void queen(int row,int n){    
-    int column;    
-    for(column=1;column<=n;column++)    {        
-        if(place(row,column))        {            
-            board[row]=column;             
-            if(row==n)                 
-                print(n);             
-            else                 
-                queen(row+1,n);        
-        }    
+int getmarkedcolumn(int row) {
+  for (int i = 0; i < n; i++) {
+    if (board[row][i] == 'Q') {
+      return i;
+      break;
     }
+  }
+  return -999; // not feasible
 }
 
-int main(){    
-    int n;    
-    printf("Enter number of Queens:");    
-    scanf("%d",&n);    
-    queen(1,n);
+int feasible(int row, int col) {
+  int c2;
+  for (int i = 0; i < n; i++) {
+    c2 = getmarkedcolumn(i);
+    if (col == c2 || abs(row - i) == abs(col - c2)) return 0; // cant be placed
+  }
+  return 1; // can place queen
+}
+
+void nqueen(int row) {
+   int i, j;
+   if (row < n) {
+      for (i = 0; i < n; i++) {
+         if (feasible(row, i)) {
+            board[row][i] = 'Q';
+            nqueen(row + 1);
+            board[row][i] = '-';
+         }
+      }
+   } else {
+      printf("\nThe solution is:- ");
+      display();
+   }
+}
+ 
+
+int main() {
+  printf("\n Enter the number of queens : ");
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      board[i][j] = '-';
+    }
+  }
+  nqueen(0); // run code from 0th
+  return 0;
 }
--- sem_5/daa/11.c
+++ sem_5/daa/11.c
@@ -1,60 +1,75 @@
 // SUM OF SUBSET
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+// SubsetSum(arr, size, sum) = SubsetSum(arr, size-1, sum) || SubsetSum(arr, size-1, sum-arr[size-1])
+// Base Cases:
+// SubsetSum(arr, size, sum) = false, if sum > 0 and size == 0
+// SubsetSum(arr, size, sum) = true, if sum == 0 
 
 #define TRUE 1
 #define FALSE 0
+int include[50], arr[50], target, n;
 
-int inc[50],w[50],sum,n;
-
-int promising(int i,int wt,int total){ 
-    return(((wt+total)>=sum)&&((wt==sum)||(wt+w[i+1]<=sum)));
+int promising(int i, int currSum, int sum) {
+    // if current_val + cummulative_sum >= target : possible answer
+    // if wt == target (ans found) OR wt + next element <= target (possible answer found)
+    return ((currSum + sum) >= target) && ((currSum == target) || (currSum + arr[i + 1] <= target));
 }
 
-void sumset(int i,int wt,int total){ 
-    int j; 
-    if(promising(i,wt,total)){  
-        if(wt==sum)   {    
-            printf("\n{\t");   
-            for (j=0;j<=i;j++)           
-                if(inc[j])    
-                    printf("%d\t",w[j]);   
-                printf("}\n");  
-        }   
-        else{   
-            inc[i+1]=TRUE;   
-            sumset(i+1,wt+w[i+1],total-w[i+1]);   
-            inc[i+1]=FALSE;   
-            sumset(i+1,wt,total-w[i+1]);  
-        } 
+void subset(int i, int currSum, int sum) {
+    if (promising(i, currSum, sum)) {
+        if (currSum == target) { // ans found
+            printf("\n { \t");
+            for (int j = 0; j <= i; j++) { // loop tricky
+                if (include[j]) {
+                    printf("%d \t", arr[j]);
+                }
+            }
+            printf("} \n");
+        } else {
+            // include next
+            include[i + 1] = TRUE;
+            subset(i + 1, currSum + arr[i + 1], sum - arr[i + 1]);
+            // not include
+            include[i + 1] = FALSE;
+            subset(i + 1, currSum, sum - arr[i + 1]);
+        }
     }
 }
 
-int main(){ 
-    int i, j, n, temp, total=0; 
-    printf("\n Enter how many numbers:\n"); 
-    scanf("%d",&n); printf("\n Enter %d numbers to the set:\n",n); 
-    for (i=0;i<n;i++)  {  
-        scanf("%d",&w[i]);  
-        total+=w[i]; 
-    } 
-    printf("\n Input the sum value to create sub set:\n"); 
-    scanf("%d",&sum); for (i=0;i<=n;i++)          
-    for (j=0;j<n-1;j++)           
-        if(w[j]>w[j+1])   {          
-            temp=w[j];          
-            w[j]=w[j+1];          
-            w[j+1]=temp;         
-        } 
-        printf("\n The given %d numbers in ascending order:\n",n); 
-        for (i=0;i<n;i++)     
-            printf("%d \t",w[i]); 
-            if((total<sum))     
-                printf("\n Subset construction is not possible");  
-            else  {     
-                for(i=0;i<n;i++)         
-                    inc[i]=0;     
-                printf("\n The solution using backtracking is:\n");     
-                sumset(-1,0,total); 
+int main() {
+    int n, temp;
+    int sum = 0;
+    printf("Enter numbers : ");
+    scanf("%d", &n);
+    printf("Enter numbers to the set : ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    printf("Enter target : ");
+    scanf("%d", &target);
+    // sort
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
+        }
+    }
+    // actual calculation
+    if (sum < target) printf("\n Not possible"); // cant happen
+    else
+    {
+        // set include arr to 0 (init)
+        for (int i = 0; i < n; i++) {
+            include[i] = 0;
+        }
+        printf("Solution : \n");
+        subset(-1, 0, sum);
+    }
+    return 0;
 }
