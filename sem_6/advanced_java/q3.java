3599fefd8ac97fa86a9ce2e95d1f62718a77d32a
rishabhpreethan <rishabh.preethan@gmail.com>
2023-03-31 15:05:18 +0530

    committed

---
+++ sem_6/advanced_java/q3.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class q3 extends Frame implements WindowListener,ActionListener {
+	private static final long serialVersionUID = 1L;
+	TextField text = new TextField(20);
+	Button b;
+	private int numClicks = 0;
+	public static void main(String[] args) {
+		q3 myWindow = new q3("Button Click Count Window");
+		myWindow.setSize(350,100);
+		myWindow.setVisible(true);
+	}
+	public q3(String title) {
+		super(title);
+		setLayout(new FlowLayout());
+		addWindowListener(this);
+		b = new Button("Click me");
+		add(b);
+		add(text);
+		b.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent e) {
+		numClicks++;
+		text.setText("Click count: " + numClicks);}
+		public void windowClosing(WindowEvent e) {
+		dispose();
+		System.exit(0);
+	}
+	public void windowOpened(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+                                                                                                                                                          public void windowClosed(WindowEvent e) {}
+}
---
+++ sem_6/advanced_java/q4.java
@@ -0,0 +1,34 @@
+package four;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class q4{
+	public static void main(String[] args) {
+		int id=0,salary=0;
+		String name,dept,s;
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from emptable");
+			System.out.println("employees");
+			System.out.println("id " + "\t" + "name" + "\t" + "department" + "\t" + "salary");
+			if(rs!=null){
+				while(rs.next()){
+					id=rs.getInt(1);
+					name=rs.getString(2);
+					dept=rs.getString(3);
+					salary=rs.getInt(4);
+					System.out.println(id + "\t" + name + "\t" + dept + "\t" + salary);
+				}
+			}
+			con.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
