e842cf834ad284dd98281d353e13953ae5a4b38f
rishabhpreethan <rishabh.preethan@gmail.com>
2023-05-23 08:20:25 +0530

    internal 1

---
+++ sem_6/location_fence/app/src/main/java/com/example/location_fence/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.location_fence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
