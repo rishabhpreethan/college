16060747800734cdac4d2981e8b7af546f4b5bb9
rishabhpreethan <rishabh.preethan@gmail.com>
2023-12-27 06:12:04 +0530

    new code

--- sem_7/computer_networks/partA_python/q5/byte_stuffing.py
+++ sem_7/computer_networks/partA_python/q5/byte_stuffing.py
@@ -1,43 +1,18 @@
 def char_stuffing():
     og_data = input("Enter the characters: ")
+    print("Original data:\n", og_data)
 
-    print("Original data:")
-    print(og_data)
+    t_data = "dlestx" + "".join(["dle" + og_data[i] if og_data[i:i+3] == "dle" else og_data[i] for i in range(len(og_data))]) + "dleetx"
 
-    t_data = "dlestx"
+    print("\nTransmitted data:\n", t_data)
 
-    i = 0
-    j = 6
-    while i < len(og_data):
-        if og_data[i:i+3] == "dle":
-            t_data += "dle"
-            i += 3
-        t_data += og_data[i]
-        i += 1
+    recieve_data = "".join([t_data[i] if t_data[i:i+6] != "dledle" else t_data[i+3] for i in range(6, len(t_data)-6)])
 
-    og_length = len(t_data)
-    og_length += 6  # dle-etx done
-    t_data += "dleetx"
-
-    print("\nTransmitted data:")
-    print(t_data)
-
-    recieve_data = ""
-
-    i = 6
-    while i < og_length - 6:
-        if t_data[i:i+6] == "dledle":
-            i += 3
-        recieve_data += t_data[i]
-        i += 1
-
-    print("\nReceived data:")
-    print(recieve_data)
+    print("\nReceived data:\n", recieve_data)
 
 if __name__ == "__main__":
     while True:
-        print("\n 1. Character stuffing")
-        print(" 2. Exit")
+        print("\n 1. Character stuffing\n 2. Exit")
         choice = int(input("Enter choice: "))
 
         if choice == 1:
